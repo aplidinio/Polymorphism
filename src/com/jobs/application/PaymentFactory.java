59f9b49368f7f0d51bd471c7433a35d6749bd1f2
Francisco <aplidinio@gmail.com>
2019-07-29 11:17:53 +0200

    20190729a

--- src/com/jobs/application/PaymentFactory.java
+++ src/com/jobs/application/PaymentFactory.java
@@ -18,6 +18,22 @@ public class PaymentFactory {
 		return new IPaymentRate() {
 			@Override
 			public double pay(double salaryPerMonth) {
+				return salaryPerMonth*0.85;//todo 
+			}
+		};
+	}
+	public static IPaymentRate createPaymentRateManager(){
+		return new IPaymentRate() {
+			@Override
+			public double pay(double salaryPerMonth) {
+				return salaryPerMonth*1.1;//todo 
+			}
+		};
+	}
+	public static IPaymentRate createPaymentRateVolunteer(){
+		return new IPaymentRate() {
+			@Override
+			public double pay(double salaryPerMonth) {
 				return 0;//todo 
 			}
 		};
